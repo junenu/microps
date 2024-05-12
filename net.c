8b20917b31a02716ebe790887ee43935b1da78b3
junenu <junenu41@gmail.com>
2024-05-12 17:04:13 +0900

    chore(step4): update net.c

--- net.c
+++ net.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <string.h>
 
 #include "platform.h"
 
 #include "util.h"
 #include "net.h"
+#include "ip.h"
+
+struct net_protocol {
+    struct net_protocol *next;
+    uint16_t type;
+    struct queue_head queue; /* input queue */
+    void (*handler)(const uint8_t *data, size_t len, struct net_device *dev);
+};
+
+struct net_protocol_queue_entry {
+    struct net_device *dev;
+    size_t len;
+    uint8_t data[];
+};
 
 static struct net_device *devices;
+static struct net_protocol *protocols;
 
 struct net_device *
 net_device_alloc(void)
@@ -91,11 +107,58 @@ net_device_output(struct net_device *dev, uint16_t type, const uint8_t *data, si
     return 0; 
 }
 
+/* NOTE: must not be call after net_run() */
+int
+net_protocol_register(uint16_t type, void (*handler)(const uint8_t *data, size_t len, struct net_device *dev))
+{
+    struct net_protocol *proto;
+
+    for (proto = protocols; proto; proto = proto->next) {
+        if (type == proto->type) {
+            errorf("already registered, type=0x%04x", type);
+            return -1;
+        }
+    }
+    proto = memory_alloc(sizeof(*proto));
+    if (!proto) {
+        errorf("memory_alloc() failure");
+        return -1;
+    }
+    proto->type = type;
+    proto->handler = handler;
+    proto->next = protocols;
+    protocols = proto;
+    infof("registered, type=0x%04x", type);
+    return 0;
+}
+
+
 int
 net_input_handler(uint16_t type, const uint8_t *data, size_t len, struct net_device *dev)
 {
-    debugf("dev=%s, type=0x%04x, len=%zu\n", dev->name, type, len);
-    debugdump(data, len);
+    struct net_protocol *proto;
+    struct net_protocol_queue_entry *entry;
+
+    for (proto = protocols; proto; proto = proto->next) {
+        if (type == proto->type) {
+            // 新しいエントリのメモリを確保
+            entry = memory_alloc(sizeof(*entry) + len);
+            if (!entry) {
+                errorf("memory_alloc failed\n");
+                return -1;
+            }
+        // 新しいエントリのメタデータの設定と受信データのコピー
+        entry->dev = dev;
+        entry->len = len;
+        memcpy(entry->data, data, len); // data から entry->data へ len バイトのデータをコピー
+        // キューにエントリを追加
+        queue_push(&proto->queue, entry);
+        debugf("queue pushed (num=%u), type=0x%04x, len=%zu\n", proto->queue.num, type, len);
+        debugdump(data, len);
+        return 0;
+        }
+    }
+    /* unsupported protocol */
     return 0;
 }
 
@@ -135,6 +198,10 @@ net_init(void)
 {
     if (intr_init() == -1) {
         errorf("intr_init failed\n");
+        return -1;
+    }
+    if (ip_init() == -1) {
+        errorf("ip_init failed\n");
         return -1;
     }
     infof("initializing...\n");
